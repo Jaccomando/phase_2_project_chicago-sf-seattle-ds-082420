7b416f8242c7430313b44a15146ef398f3ce60dd
Matthew Williamson <matthew.l.williamson.96@gmail.com>
2020-10-01 12:13:50 -0700

    continued work on model exploration, added functions that auto produce models and heatmaps

--- src/data_download.py
+++ src/data_download.py
@@ -2,7 +2,39 @@ from io import BytesIO, TextIOWrapper, StringIO
 from zipfile import ZipFile
 import pandas as pd
 import requests
-import pandas as pd
+import numpy as np
+import scipy
+import seaborn as sns
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+from statsmodels.regression.linear_model import GLS
+
+
+def produce_model(data, target):
+    '''
+    takes data as a dataframe of predictors with a target, takes target as a string equal to the column name
+    of the target in data.
+    returns the model generated from smf.ols().fit()
+    '''
+    preds = f'{target} ~ ' + str(data.loc[ : , data.columns != target].columns[0])
+    for pred in data.loc[ : , data.columns != target].columns[1:]:
+        preds = preds + f' + {pred}'
+    formula = preds
+
+    model = smf.ols(preds, data).fit()
+    return model
+
+def make_heatmap(data):
+    mask = np.triu(np.ones_like(data.corr(), dtype=np.bool))
+    fig, ax = plt.subplots(figsize = (15,10))
+    plt.xticks(fontsize = 10)
+    plt.yticks(fontsize = 10)
+    sns.heatmap(data.corr(), mask= mask, ax= ax, cmap = 'viridis')
+    return ax
 
 def get_tables(dictionary):
     
